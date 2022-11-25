d126723b7b62bdd1719a03aca69d5e4e2d944f99
fabioalvarez <fabio.alvarezf@icloud.com>
2022-11-25 23:55:14 +0000

    Updating project

--- model/ml_service.py
+++ model/ml_service.py
@@ -1,8 +1,30 @@
-def predict():
-    pass
+from app.detect import parse_opt
+from app.detect import main
+from dotenv import load_dotenv
+import json
+import yaml
+import os
+
+load_dotenv()
+
+# Load env variables
+database = os.getenv('DATABASE')
+project = os.getenv('PROJECT')
+weights = os.getenv('WEIGTHS')
+save_txt = os.getenv('SAVE_TXT')
+source = "/home/src/app/data/images/zidane.jpg"
 
 
-if __name__ == "__main__":
-    # Now launch process
-    print("Launching ML service...")
-    predict()
+# Get the preset values from the parse function
+args = parse_opt()
+
+# Change values
+args.save_conf = True
+args.save_txt = True
+args.project = project
+# args.weights = weights
+args.source = source
+args.name = "zidane"
+
+run = main(args)
+
